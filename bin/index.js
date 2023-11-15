8d50e44b72ef1014b898ec3cdd27511d45a902d4
ICe1BotMaker <ice1github@gmail.com>
2023-11-15 18:29:20 +0900

    fixed

--- bin/index.js
+++ bin/index.js
@@ -6,17 +6,9 @@ const command = `build`;
 if (command === `build`) {
     console.log(`${chalk.gray(`┌`)} ${chalk.gray(`linterf-scripts`)} ▞ ${chalk.hex(`#00ADB5`)(command)} ${chalk.gray(`┐`)}`);
 
-    exec(`mkdir xcopy`, { cwd: __dirname.replace(`\\bin`, ``) }, (error) => {
-        if (!error) exec(`xcopy ${process.argv[1]} ${__dirname.replace(`\\bin`, ``)}\\xcopy /E`, { cwd: `./` }, (error) => {
-            if (!error) console.log(`${chalk.gray(`└┄>`)} build start`);
-    
-            if (!error) exec(`npx --plugins=transform-jsx-syntax.js babel xcopy --out-dir build`, { cwd: __dirname.replace(`\\bin`, ``) }, (error) => {
-                if (!error) console.log(`${chalk.gray(`└┄>`)} success.`);
+    console.log(`${chalk.gray(`└┄>`)} build start`);
 
-                if (!error) exec(`rmdir /s /q xcopy`, { cwd: __dirname.replace(`\\bin`, ``) }, (error) => {
-                    if (!error) exec(`node .`, { cwd: __dirname.replace(`\\bin`, `\\build`) });
-                });
-            });
-        });
+    exec(`npx babel .\\ --out-dir ${__dirname.replace(`\\bin`, ``)}\\build`, { cwd: `./` }, (error) => {
+        if (!error) console.log(`${chalk.gray(`└┄>`)} success.`);
     });
 }
